bd3f33ec25c1221e581d5cae96aa7913100d598d
Adam Zidiker <adam@cornix.io>
2025-08-03 18:45:05 +0300

    Adding cornix errors

--- cornix_ccxt/errors.py
+++ cornix_ccxt/errors.py
@@ -1,5 +1,27 @@
-from ccxt.base.errors import InvalidOrder
+from ccxt.base.errors import (BaseError, ExchangeError, InvalidOrder,
+                              RateLimitExceeded)
 
 
 class TradesNotFound(InvalidOrder):
+    pass
+
+
+class OrderCancelled(InvalidOrder):
+    """Raised when you are trying to fetch or cancel a non-existent order"""
+    pass
+
+
+class MaxStopAllowed(ExchangeError):
+    """"Raised when an exchange server replies with an error in JSON"""
+    pass
+
+class PositionNotFound(ExchangeError):
+    pass
+
+
+class NotChanged(BaseError):
+    pass
+
+
+class AccountRateLimitExceeded(RateLimitExceeded):
     pass
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='cornix_ccxt',
-    version='1.0.9',
+    version='1.0.0',
     packages=find_packages(),
     install_requires=[
         'ccxt @ https://github.com/CornixIO/ccxt/archive/refs/tags/4.0.106.95.tar.gz#subdirectory=python',
